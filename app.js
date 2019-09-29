d734c4ba25810e4d26e515fd1686196ad666c72f
Adam <adam.orlowski@rtbhouse.com>
2019-09-29 23:36:21 +0200

    add readStream

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const http = require('http');
+const fs = require('fs');
 
-const server = http.createServer(((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World! Send from a server');
-}));
+const readStream = fs.createReadStream(`${__dirname}/readme.txt`, 'utf8');
+readStream.on('data', chunk => {
+  console.log('New chunk received');
+});
 
-server.listen(3000, '127.0.0.1');
-console.log('Server is listening on port 3000');
+
+// const server = http.createServer(((req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'text/plain' });
+//   res.end('Hello World! Send from a server');
+// }));
+//
+// server.listen(3000, '127.0.0.1');
+// console.log('Server is listening on port 3000');
