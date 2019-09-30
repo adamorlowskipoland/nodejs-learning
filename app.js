9de901614b09b6f6ef149f6b8c63a1a697a02e16
Adam <adam.orlowski@rtbhouse.com>
2019-09-30 13:55:59 +0200

    render index.html from node server

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer(((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  const readStream = fs.createReadStream(`${__dirname}/readme.txt`, 'utf8');
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  const readStream = fs.createReadStream(`${__dirname}/index.html`, 'utf8');
   readStream.pipe(res);
 }));
 
