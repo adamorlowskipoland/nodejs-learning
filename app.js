46507f539b06bc950319c82b665f1a83159d6209
Adam <adam.orlowski@rtbhouse.com>
2019-09-30 14:19:16 +0200

    create basic routing

--- app.js
+++ app.js
@@ -2,13 +2,48 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer(((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  const obj = {
-    name: 'Ryu',
-    job: 'Ninja',
-    age: 29,
-  };
-  res.end(JSON.stringify(obj));
+  console.log(`request was made: ${req.url}`);
+  if (req.url === '/home' || req.url === '/') {
+    res.writeHead(
+      200,
+      { 'Content-Type': 'text/html' }
+    );
+    fs
+      .createReadStream(__dirname + '/index.html', 'utf8')
+      .pipe(res);
+  } else if (req.url === '/contact') {
+    res.writeHead(
+      200,
+      { 'Content-Type': 'text/html' }
+    );
+    fs
+      .createReadStream(__dirname + '/contact.html', 'utf8')
+      .pipe(res);
+  } else if (req.url === '/api/ninjas') {
+    const ninjas = [
+      {
+        name: 'Ryu',
+        age: 29,
+      },
+      {
+        name: 'Yoshi',
+        age: 32,
+      },
+    ];
+    res.writeHead(
+      200,
+      { 'Content-Type': 'application/json' }
+    );
+    res.end(JSON.stringify(ninjas));
+  } else {
+    res.writeHead(
+      404,
+      { 'Content-Type': 'text/html' }
+    );
+    fs
+      .createReadStream(__dirname + '/404.html', 'utf8')
+      .pipe(res);
+  }
 }));
 
 server.listen(3000, '127.0.0.1');
