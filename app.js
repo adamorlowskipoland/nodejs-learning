512a0815c43c5e78713d73dbf6cbe7d44002cf45
Adam <adam.orlowski@rtbhouse.com>
2019-09-30 14:00:35 +0200

    serve json obj as response

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer(((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  const readStream = fs.createReadStream(`${__dirname}/index.html`, 'utf8');
-  readStream.pipe(res);
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  const obj = {
+    name: 'Ryu',
+    job: 'Ninja',
+    age: 29,
+  };
+  res.end(JSON.stringify(obj));
 }));
 
 server.listen(3000, '127.0.0.1');
