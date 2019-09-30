88a528e3b0bcd29d049bba9677e8223effb75c04
Adam <adam.orlowski@rtbhouse.com>
2019-09-30 13:45:42 +0200

    use pipe to write content to response in chunks

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 const http = require('http');
 const fs = require('fs');
 
-const readStream = fs.createReadStream(`${__dirname}/readme.txt`, 'utf8');
-const writeStream = fs.createWriteStream(`${__dirname}/writeme.txt`,);
+const server = http.createServer(((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  const readStream = fs.createReadStream(`${__dirname}/readme.txt`, 'utf8');
+  readStream.pipe(res);
+}));
 
-readStream.pipe(writeStream);
-
-
-// const server = http.createServer(((req, res) => {
-//   res.writeHead(200, { 'Content-Type': 'text/plain' });
-//   res.end('Hello World! Send from a server');
-// }));
-//
-// server.listen(3000, '127.0.0.1');
-// console.log('Server is listening on port 3000');
+server.listen(3000, '127.0.0.1');
+console.log('Server is listening on port 3000');
