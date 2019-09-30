da8584265bcb0d98cee7671a4dd43cf9801acf8b
Adam <adam.orlowski@rtbhouse.com>
2019-09-30 16:29:28 +0200

    render html files using .sendFile

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('this is the home page')
+  res.sendFile(`${__dirname}/index.html`);
 });
 
 app.get('/contact', (req, res) => {
-  res.send('this is the contact page')
+  res.sendFile(`${__dirname}/contact.html`);
 });
 
 app.get('/profile/:id', (req, res) => {
