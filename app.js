c91d14a03fa0c72145419159b9bfa899652f1f32
Adam <adam.orlowski@rtbhouse.com>
2019-09-30 16:19:48 +0200

    routing with express - simple

--- app.js
+++ app.js
@@ -1,50 +1,12 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const app = express();
 
-const server = http.createServer(((req, res) => {
-  console.log(`request was made: ${req.url}`);
-  if (req.url === '/home' || req.url === '/') {
-    res.writeHead(
-      200,
-      { 'Content-Type': 'text/html' }
-    );
-    fs
-      .createReadStream(__dirname + '/index.html', 'utf8')
-      .pipe(res);
-  } else if (req.url === '/contact') {
-    res.writeHead(
-      200,
-      { 'Content-Type': 'text/html' }
-    );
-    fs
-      .createReadStream(__dirname + '/contact.html', 'utf8')
-      .pipe(res);
-  } else if (req.url === '/api/ninjas') {
-    const ninjas = [
-      {
-        name: 'Ryu',
-        age: 29,
-      },
-      {
-        name: 'Yoshi',
-        age: 32,
-      },
-    ];
-    res.writeHead(
-      200,
-      { 'Content-Type': 'application/json' }
-    );
-    res.end(JSON.stringify(ninjas));
-  } else {
-    res.writeHead(
-      404,
-      { 'Content-Type': 'text/html' }
-    );
-    fs
-      .createReadStream(__dirname + '/404.html', 'utf8')
-      .pipe(res);
-  }
-}));
+app.get('/', (req, res) => {
+  res.send('this is the home page')
+});
 
-server.listen(3000, '127.0.0.1');
-console.log('Server is listening on port 3000');
+app.get('/contact', (req, res) => {
+  res.send('this is the contact page')
+});
+
+app.listen(3000);
