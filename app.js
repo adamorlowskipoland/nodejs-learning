3bd20f9f8cea289c00c2e59ce7f98d3eb0950e36
Adam <adam.orlowski@rtbhouse.com>
2019-10-02 16:15:06 +0200

    serve static files through middleware with express.static()

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
 
 app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
+  res.render('index');
 });
 
 app.get('/contact', (req, res) => {
-  res.sendFile(`${__dirname}/contact.html`);
+  res.render('contact');
 });
 
 app.get('/profile/:name', (req, res) => {
